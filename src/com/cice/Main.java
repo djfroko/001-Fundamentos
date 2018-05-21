bfa9720def58743be646fe2463129f2b252ef8e9
cice <cice@A8POVMac12.local>
2018-05-21 21:55:11 +0200

    Initial commit

---
+++ src/com/cice/Main.java
@@ -0,0 +1,38 @@
+package com.cice;
+
+public class Main {
+
+    /**
+     * Esto es un comentario para la documentacion javaDoc
+     * @param args
+     */
+
+    public static void main(String[] args) {
+
+         int numero = 0;
+         numero = 10;
+
+         final int CONSTANTE = 12;
+
+         int a = 2;
+         System.out.println(a*a);
+
+         
+         saluda("yo");
+
+
+
+    }
+        public static String saluda(String nombre){
+
+        int dato = (1==2) ? 1 : 2;
+        return "Hola chicos";
+
+        }
+
+
+
+
+
+
+}
