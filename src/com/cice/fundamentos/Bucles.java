9598625f6225267c5ee83cdefb00f07109cd0899
djfroko <djfroko@gmail.com>
2018-05-30 21:50:08 +0200

    ejercicio 3.1

---
+++ src/com/cice/fundamentos/Bucles.java
@@ -0,0 +1,15 @@
+package com.cice.fundamentos;
+
+public class Bucles {
+
+    public static void main(String[] args) {
+
+        //BUCLE FOR
+
+        //for (iniciacion; condicion; incremento){
+        for(int i = 0; i<10; i++){
+            //ejecutamos codigo,
+            System.out.println(i);
+        }
+    }
+}
