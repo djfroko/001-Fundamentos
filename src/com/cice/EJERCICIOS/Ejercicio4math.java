06f9ced32d484a4c42e4027a69407e2a010423d8
djfroko <djfroko@gmail.com>
2018-06-26 12:19:49 +0200

    ejercicio 4 math

---
+++ src/com/cice/EJERCICIOS/Ejercicio4math.java
@@ -0,0 +1,37 @@
+package com.cice.EJERCICIOS;
+
+/*En este ejercicio vamos a realizar un programa que nos muestre la suma de todos los números
+enteros comprendidos entre dos números aleatorios generados por el propio programa. Los
+números aleatorios serán mayores de 0 y menores de 100.*/
+
+public class Ejercicio4math {
+
+    public static void main(String[] args) {
+
+        int numero1 = (int)(Math.random()*100+1);
+        int numero2 = (int)(Math.random()*100+1);
+
+        int numeroMayor = Math.max(numero1,numero2);
+        System.out.println(numeroMayor);
+
+        int numeroMenor = Math.min(numero1, numero2);
+        System.out.println(numeroMenor);
+
+        int suma = 0;
+
+
+        for ( int i = numeroMenor ; i < numeroMayor ; i++){
+
+            suma += numeroMenor;
+        }
+
+        System.out.println(suma);
+
+
+        }
+
+    }
+
+
+
+
