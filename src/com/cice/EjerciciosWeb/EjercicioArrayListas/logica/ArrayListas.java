18524a399c28968009a37b21861e7cb7de9d51ea
djfroko <djfroko@gmail.com>
2018-07-16 09:05:21 +0200

    arrays ejercicios sin resolver

--- src/com/cice/EjerciciosWeb/EjercicioArrayListas/logica/ArrayListas.java
+++ src/com/cice/EjerciciosWeb/EjercicioArrayListas/logica/ArrayListas.java
@@ -6,25 +6,32 @@ import java.util.Scanner;
 public class ArrayListas {
 
     Scanner sc = new Scanner(System.in);
-    int cantidad;
+    private int cantidad;
+    private double media = 0;
+    private int numeroAltos= 0;
+    private int numeroBajos = 0;
 
     public void iniciarApp(){
 
         numeroAlumnos();
         leerAlturas();
         calcularMedias();
+        mediaAltos();
+        mediaBajos();
         mostrarResultados();
     }
 
 
     //Calcular la altura media de los alumnos de una clase
 
-    ArrayList<Double> listaAltura = new ArrayList<>();
+    private ArrayList<Double> listaAltura = new ArrayList<>();
 
 
-    double media = 0;
+
 
     private void  numeroAlumnos() {
+
+        System.out.println("Introduce el numero de alumnos ");
 
         cantidad = sc.nextInt();
 
@@ -32,33 +39,52 @@ public class ArrayListas {
 
     private void leerAlturas() {
 
-        double alturas;
-
         for (int i = 0; i < cantidad; i++) {
 
+            System.out.println( " Introduce la altura ");
+
             listaAltura.add(sc.nextDouble());
-
         }
-
-
     }
 
     private void calcularMedias() {
 
-        for (int i = 0; i < listaAltura.size(); i++) {
+        for (int i = 0; i < cantidad ; i++) {
 
-            listaAltura.get(i) += media;
+           listaAltura.get(i) += media ;
         }
 
-        media = media / listaAltura.size();
-
+        media = media / cantidad;
     }
 
+    private void mediaAltos(){
+
+        for (int i = 0; i < cantidad; i++) {
+            if (listaAltura.get(i)>media){
+                numeroAltos++;
+            }
+
+        }
+    }
+
+    private void mediaBajos(){
+        for (int i = 0; i < cantidad; i++) {
+            if (listaAltura.get(i)<media){
+                numeroBajos++;
+            }
+
+        }
+
+    }
 
     private void mostrarResultados() {
 
         System.out.println("===============================");
-        System.out.print("La media de las notas es: " + media);
+        System.out.println("La media de la altura es: " + media);
+        System.out.println("El numero que supera la altura media es " + numeroAltos);
+        System.out.println("El numero por debajo de la media de altura es " + numeroBajos);
+        System.out.println("===============================");
+
     }
 
 
---
+++ src/com/cice/EjerciciosWeb/EjerciciosStrings/EjerciciosString1.java
@@ -0,0 +1,18 @@
+package com.cice.EjerciciosWeb.EjerciciosStrings;
+
+public class EjerciciosString1 {
+
+    public static void main(String[] args) {
+        /*
+         *Juego "acierta la contraseña". Pedir al jugador 1 que introduzca una palabra contraseña.
+         *Luego se mostrara al jugador 2 un mensaje con la primera y ultima letras de la contraseña.
+         *Debera intentar adivinar la contraseña. A cada intento se mostrara mensaje diciendo si el
+         *intento tiene mas o menos caracteres que la contraseña. Al acertar, mensaje de enhorabuena.
+         */
+
+
+
+
+
+    }
+}
